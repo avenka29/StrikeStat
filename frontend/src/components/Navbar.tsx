c063ae2c815047eac18a8868fb4c190d87650d76
Farhad Bhatti <farhadhaider5@gmail.com>
2025-06-15 19:46:07 -0400

    Added home menu item without dropdown

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,27 +7,33 @@ import {
   NavigationMenuTrigger
 } from "@/components/ui/navigation-menu";
 
+
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <NavigationMenu>
       <NavigationMenuList>
+        {/* Home - simple link, no dropdown */}
         <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link to="/docs">Docs</Link>
+          <NavigationMenuLink href="/" className="p-2">
+            Home
           </NavigationMenuLink>
         </NavigationMenuItem>
+
+
+      
+        {/* Fighters - dropdown item */}
         <NavigationMenuItem>
           <NavigationMenuTrigger>Fighters</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
+            <NavigationMenuLink href="/fighters">Link</NavigationMenuLink>
           </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
-  )
-}
+  );
+};
 
 export default Navbar
 
