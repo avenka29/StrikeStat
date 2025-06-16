034170021c03644b98ccda8b3cd2209529db9d4e
Adith Venkatesh <avenkatesh081@gmail.com>
2025-06-15 21:30:24 -0400

    navbar updates

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,25 +11,32 @@ import {
 
 const Navbar = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/" className="font-bold">
-            Home
-          </NavigationMenuLink>
-        </NavigationMenuItem>
+    <div className="flex justify-between w-full ml-4 my-2">
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList className="">
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/" className="font-bold text-lg">
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="font-bold">Fighters</NavigationMenuTrigger>
-            <NavigationMenuContent>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="font-bold text-lg">Fighters</NavigationMenuTrigger>
+            <NavigationMenuContent className="absolute z-50 mt-2">
               <NavigationMenuLink href="/fighters">Link</NavigationMenuLink>
             </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/" className="font-bold text-lg">
+              About
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 };
 
-export default Navbar
+export default Navbar;
 
