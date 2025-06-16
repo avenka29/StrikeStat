2ebe58f80224dfb18e9209501c3434c3b543c027
Adith Venkatesh <avenkatesh081@gmail.com>
2025-06-15 20:09:38 -0400

    navbar fail

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,27 +8,23 @@ import {
 } from "@/components/ui/navigation-menu";
 
 
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        {/* Home - simple link, no dropdown */}
         <NavigationMenuItem>
-          <NavigationMenuLink href="/" className="p-2">
+          <NavigationMenuLink href="/" className="font-bold">
             Home
           </NavigationMenuLink>
         </NavigationMenuItem>
 
 
-      
-        {/* Fighters - dropdown item */}
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Fighters</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink href="/fighters">Link</NavigationMenuLink>
-          </NavigationMenuContent>
+          <NavigationMenuTrigger className="font-bold">Fighters</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink href="/fighters">Link</NavigationMenuLink>
+            </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
