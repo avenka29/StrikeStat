d3bd313b61c4b68c17462852a0abc27345487681
fhbhatti <fhbhatti@ncsu.edu>
2025-06-15 21:54:59 -0400

    fixed navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,41 +4,36 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
+  NavigationMenuTrigger
 } from "@/components/ui/navigation-menu";
+
+
 
 const Navbar = () => {
   return (
-    <div className="w-full border-b-4 border-gray-300">
-      <div className="flex justify-between w-full ml-4 my-2 pb-2">
-        <NavigationMenu viewport={false}>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/" className="font-bold text-lg">
-                Home
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+    <div className="flex justify-between w-full ml-4 my-2">
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList className="">
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/" className="font-bold text-lg">
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="font-bold text-lg">
-                Fighters
-              </NavigationMenuTrigger>
-              <NavigationMenuContent className="absolute z-50 mt-2">
-                <NavigationMenuLink href="/fighters">Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="font-bold text-lg">Fighters</NavigationMenuTrigger>
+            <NavigationMenuContent className="absolute z-50 mt-2">
+              <NavigationMenuLink href="/fighters">Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
 
-            <NavigationMenuItem>
-              <NavigationMenuLink href="/" className="font-bold text-lg">
-                About
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-
-          <NavigationMenuViewport />
-        </NavigationMenu>
-      </div>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/" className="font-bold text-lg">
+              About
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 };
