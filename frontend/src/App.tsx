328417b8ec6a2be496e4ac41453d28c836be6e97
fhbhatti <fhbhatti@ncsu.edu>
2025-06-15 21:51:27 -0400

    Addded footer and bottom of navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Navbar/>
+    <div className="flex flex-col min-h-screen">
+      <header className="w-full">
+        <Navbar />
+      </header>
+
+      <main className="flex-1">
+      </main>
+
+    <Footer />
+
     </div>
-  )
+  );
 }
- 
-export default App
+
+export default App;
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white text-center py-4">
+            © 2025 StrikeStat
+        </footer>
+    )
+}
+
+export default Footer
