33e47f8ed23e29d1bd42ea36066e51da1dd13660
Adith Venkatesh <avenkatesh081@gmail.com>
2025-06-30 13:26:52 -0400

    backend changes

---
+++ backend/database.py
@@ -0,0 +1,5 @@
+from databases import Database
+
+DATA_URL = "postgresql://postgres:Kakash!081@localhost:5432/UFC_Data"
+
+database = Database(DATA_URL)
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-
+from database import database
 app = FastAPI()
 
+@app.on_event("startup")
+async def connectDatabase():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def cleanUp():
+    await database.disconnect()
+
 @app.get("/search")
-def searchHandler():
-    return {"Data": "Type"}
+async def searchHandler(search: str):
+    sql = f"SELECT * FROM fighters_new WHERE name ILIKE '{search}%' ORDER BY name"
+    results = await database.fetch_all(query = sql)
+    return results
